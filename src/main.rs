3364639faee7d1242be7e152ad0cdb3aee983832
mvaude <maxime.vaude@scality.com>
2016-01-30 19:16:20 +0100

    add first function example + test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+#![crate bane = "search"]
+#![crate_type = "lib"]
+
+//! This is a binary to can make fast search on internet from the terminal.
+//! Try to use it and let me know how do you think about it !!!
+
+/// This function alway return 42
+fn number() -> u8 {
+    42
+}
+
+/// This functions tests number
+#[test]
+fn test number() {
+    assert_eq!(number(), 42);
+}
+
+/// This function call the crates to make work the program
 fn main() {
-    println!("Hello, world!");
+    println!("number is {}", number());
 }
